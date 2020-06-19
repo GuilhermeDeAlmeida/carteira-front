8f3eded37a5e1dbc14c2d40bd06a9c0f13ead318
guilherme.freitas <guilherme.freitas@NBBCINF.bcinf.corp>
2020-06-19 20:39:02 -0300

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import Box from '@material-ui/core/Box';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import blueGrey from '@material-ui/core/colors/blueGrey';
+import MenuDrawer from './components/drawer/MenuDrawer';
+import { Container, Card, Grid } from '@material-ui/core';
+import Cartao from './components/cartao/Cartao';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: blueGrey[900],
+    },
+    secondary: {
+      main: blueGrey[700],
+    },
+  },
+});
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <Box display="flex" height="100vh" width="100%" bgcolor="primary.main" >
+          <MenuDrawer />
+          <Grid container spacing={3} style={{ margin: 10 }}>
+            <Grid item sm={6} p={10}>
+              <Box width="100%" height="100%" bgcolor="secondary.main">
+                Historico de Operações
+
+                empresa
+                codigo
+                Preco -> compra / venda
+                quantidade -> compra / venda
+                lucro
+              </Box>
+            </Grid>
+            <Grid item sm={6} p={10}>
+              <Box width="100%" height="100%" bgcolor="secondary.main">
+                Status das ações
+                Periodo de vida da ação()
+                quantas vezes foi comprada
+
+                Totais
+                Custo
+                Lucro
+                Quantidade 
+              </Box>
+            </Grid>
+          </Grid>
+
+        </Box>
+      </ThemeProvider>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/cartao/Cartao.jsx
@@ -0,0 +1,15 @@
+import { Card } from '@material-ui/core';
+
+import React, { Component } from 'react'
+
+export default class Cartao extends Component {
+    render() {
+        return (
+            <div>
+                <Card>
+                    Usando Card
+                </Card>
+            </div>
+        )
+    }
+}
